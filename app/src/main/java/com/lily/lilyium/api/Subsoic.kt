db48f230d872528aadf21961e9773e126a635a86
lily5463 <kronglykoko2@gmail.com>
2026-07-09 02:25:30 +0700

    Restore main from dev

--- app/src/main/java/com/lily/lilyium/api/Subsoic.kt
+++
@@ -1,4 +0,0 @@
-package com.lily.lilyium.api
-
-class Subsoic {
-}
--- app/src/main/java/com/lily/lilyium/mediaPlayer/MusicService.kt
+++
@@ -1,9 +0,0 @@
-package com.lily.lilyium.mediaPlayer
-//TODO: Implement MediaPlayer class
-
-//import android.media.MediaPlayer
-//
-//class MusicService : Service() {
-//    private var mediaPlayer: MediaPlayer? = null
-//
-//}
